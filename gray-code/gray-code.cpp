61f8a147e30da22936845df901f81326897981d6
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2021-07-02 17:26:03 +0530

    Time: 8 ms (78.26%), Space: 12 MB (45.81%) - LeetHub

--- gray-code/gray-code.cpp
+++ gray-code/gray-code.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> res = {0};
-        int size;
-        
-        for (int i = 0; i < n; i++) {
-            size = res.size();
-            for (int j = size - 1; j >= 0; j--) {
-                res.push_back(res[j] | 1 << i);
-            }
+       vector<int> v;
+        long long int p=pow(2,n);
+        for(int i=0; i<p; i++){
+            v.push_back(i^(i/2));
         }
-        return res;
+        return v;
     }
 };
