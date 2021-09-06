701292e163d72ff24f001d04da8b96769b08803a
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2021-09-06 16:32:14 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (48.36%) - LeetHub

---
+++ n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int t[n+4];
+        t[0]=0;
+        t[1]=1;
+        t[2]=1;
+        
+        for(int i=3;i<=n;i++)
+        {
+            t[i]= t[i-1]+t[i-2]+t[i-3];
+        }
+        return t[n];
+    }
+};
