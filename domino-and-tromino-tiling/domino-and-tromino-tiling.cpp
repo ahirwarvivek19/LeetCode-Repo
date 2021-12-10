fa10deb500b56bcd3c032bea41e622b2e7c51271
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2021-12-11 00:56:16 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.89%) - LeetHub

--- domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -1,21 +1,20 @@
 #define mod 1000000007
-
 class Solution {
 public:
-    
     int numTilings(int n) {
-        vector<long long >dp(n+1,0);
         if(n==1) return 1;
-        if(n==2) return 2;                            //Base Cases
+        if(n==2) return 2;                            
         if(n==3) return 5;
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=5;
+        vector<long long> dp(n+1,0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
         for(int i=4;i<=n;i++)
         {
-            dp[i]=((2*dp[i-1])%mod+(dp[i-3])%mod)%mod;
+            dp[i] = (2*(dp[i-1])%mod+(dp[i-3])%mod)%mod;
         }
         return dp[n];
+        
     }
 };
