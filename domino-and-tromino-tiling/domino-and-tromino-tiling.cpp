d07b9b70eec0d84a6741d1184bdc113adbb58a3a
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2021-12-30 21:16:36 +0530

    Time: 7 ms (11.66%), Space: 7.5 MB (10.86%) - LeetHub

--- domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -1,20 +1,48 @@
 #define mod 1000000007
 class Solution {
+    int nn;
+    // const int mod = 1e9+7;
+private:
+    int f(int ind,bool sp,vector<vector<int>> &dp)
+    {
+        if(ind==nn+1)
+        {
+            if(sp==false)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        if(ind>nn)
+        {
+           return 0; 
+        }
+        
+        if(dp[ind][sp]!=-1)
+            return dp[ind][sp];
+        
+        long long cnt = 0;
+        if(sp)
+        {
+            cnt+=f(ind+1,false,dp);
+            cnt+=f(ind+1,true,dp);
+        }
+        else
+        {
+            cnt+=f(ind+1,false,dp);
+            cnt+=f(ind+2,false,dp);
+            cnt+=2*f(ind+2,true,dp);
+        }
+        return dp[ind][sp] = cnt%mod;
+        
+    }
 public:
     int numTilings(int n) {
-        if(n==1) return 1;
-        if(n==2) return 2;                            
-        if(n==3) return 5;
-        vector<long long> dp(n+1,0);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 5;
-        for(int i=4;i<=n;i++)
-        {
-            dp[i] = (2*(dp[i-1])%mod+(dp[i-3])%mod)%mod;
-        }
-        return dp[n];
-        
+        nn=n;
+        vector<vector<int>> dp(n+2,vector<int>(2,-1));
+        return f(1,false,dp);
     }
 };
