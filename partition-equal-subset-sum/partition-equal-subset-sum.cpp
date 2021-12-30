8921545824ab3faa24387c30dd531b659b68dd4c
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2021-12-30 21:54:07 +0530

    Time: 224 ms (29.53%), Space: 75.2 MB (18.98%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,46 +1,41 @@
 class Solution {
+private:
+    bool func(int ind, int sum,vector<int> &nums,vector<vector<int>> &dp)
+    {
+        if(sum<0)
+        {
+            return 0;
+        }
+        if(ind==nums.size())
+        {
+            return sum==0;
+        }
+        
+        if(dp[ind][sum]!=-1) return dp[ind][sum];
+
+        bool pick = func(ind+1,sum-nums[ind],nums,dp);
+        
+        bool nonpick = func(ind+1,sum,nums,dp);
+        
+        return dp[ind][sum] = pick|nonpick;
+    }
 public:
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         int sum = 0;
         for(auto x:nums)
         {
             sum+=x;
         }
-        
-        if(sum%2!=0)
+        if(sum&1)
         {
             return false;
         }
-        int s = sum/2;
-        bool dp[n+1][s+1];
+        vector<vector<int>> dp(n,vector<int>(sum/2+1,-1));
+        // find subset with sum = sum/2
         
-        for(int j=0;j<=s;j++)
-        {
-            dp[0][j]=false;
-        }
+        return func(0,sum/2,nums,dp);
         
-        for(int i=0;i<=n;i++)
-        {
-            dp[i][0]=true;
-        }
-        
-        
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=s;j++)
-            {
-                if(nums[i-1]<=j)
-                {
-                    dp[i][j] = (dp[i-1][j-nums[i-1]]) || (dp[i-1][j]);
-                }
-                else
-                {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][s];
         
     }
 };
