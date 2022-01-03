171badd9dffa95b5ba7d809d450eff662b7fda5f
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2022-01-03 14:23:48 +0530

    Time: 324 ms (5.69%), Space: 68.9 MB (29.76%) - LeetHub

--- find-the-town-judge/find-the-town-judge.cpp
+++ find-the-town-judge/find-the-town-judge.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> score(n+1,0);
+        vector<int> par(n+1,0);
+        vector<int> cnt(n+1,0);
         
-        for(auto x: trust)
+        for(auto x:trust)
         {
-            score[x[0]]--;
-            score[x[1]]++;
+            cnt[x[0]]--;
+            cnt[x[1]]++;
         }
         
         for(int i=1;i<=n;i++)
         {
-            if(score[i]==n-1)
+            if(cnt[i]==n-1)
             {
                 return i;
-                
             }
         }
         return -1;
