5ed6455104bd4914be314b9601334f803b03ebba
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2022-02-14 13:29:37 +0530

    Time: 30 ms, Memory: 9.9 MB - LeetHub

--- minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& A) {
-        int n=A.size();
-        int m=A[0].size();
-        for(int i=1;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(j==0)                                                    //Leftmost or first column
-                A[i][j]+=min(A[i-1][j],A[i-1][j+1]);
-                else if(j==m-1)A[i][j]+=min(A[i-1][j],A[i-1][j-1]);     //Rightmost or last column
-                else A[i][j]+=std::min({A[i-1][j],A[i-1][j+1],A[i-1][j-1]}); //Remaining cases
+        int n = A.size();
+        int m = A[0].size();
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (j == 0) // Leftmost or first column
+                    A[i][j] += min(A[i - 1][j], A[i - 1][j + 1]);
+                else if (j == m - 1)
+                    A[i][j] += min(A[i - 1][j], A[i - 1][j - 1]); // Rightmost or last column
+                else
+                    A[i][j] += std::min({A[i - 1][j], A[i - 1][j + 1], A[i - 1][j - 1]}); // Remaining cases
             }
         }
-        int sum=INT_MAX;
-        for(int i=0;i<m;i++){
-            sum=min(sum,A[n-1][i]);                                      //Last row scan
+        int sum = INT_MAX;
+        for (int i = 0; i < m; i++)
+        {
+            sum = min(sum, A[n - 1][i]); // Last row scan
         }
         return sum;
     }
