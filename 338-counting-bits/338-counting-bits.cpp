103be94b55418e33bade49d5af1a7d00b725a548
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2022-03-01 09:11:28 +0530

    Time: 3 ms (95.58%), Space: 8 MB (48.88%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int> dp(n+1,0);
+        
+        for(int i=1;i<=n;i++)
+        {
+            dp[i] = dp[i/2]+i%2;
+        }
+        return dp;
+    }
+};
