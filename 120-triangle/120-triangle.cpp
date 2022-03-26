06813550204789d8d21553862762ec586345b69a
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2022-03-26 17:09:29 +0530

    Time: 8 ms (63.39%), Space: 9 MB (37.46%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& a) {
+        int n = a.size();
+        
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        
+        for(int j=0;j<n;j++)
+        {
+            dp[n-1][j] = a[n-1][j];
+        }
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                dp[i][j] = min(dp[i+1][j],dp[i+1][j+1])+a[i][j];
+            }
+        }
+        return dp[0][0];
+    }
+};
