299e855309749ba0b0cb3cd2198125222b4fac15
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2022-03-26 14:37:28 +0530

    Time: 60 ms (32.53%), Space: 27.7 MB (13.43%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l=0;
+        int h = n-1;
+        while(l<=h)
+        {
+            int mid = (l+(h-l)/2);
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                h= mid-1;
+            }
+            else
+            {
+                l= mid+1;
+            }
+        }
+        return -1;
+            
+    }
+};
