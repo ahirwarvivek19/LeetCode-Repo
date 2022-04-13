0288ec6578166e46bc1ca104aef1689fe27f4b89
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2022-04-14 00:30:40 +0530

    Time: 3 ms (54.28%), Space: 8.1 MB (6.79%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0)
+            return 0;
+        else if(n==1)
+            return nums[0];
+        else if(n==2)
+            return max(nums[0],nums[1]);
+        
+        vector<int> dp1(n-1,0);
+        vector<int> dp2(n-1,0);
+        
+        dp1[0] = nums[0];
+        dp1[1] = max(nums[1],dp1[0]);
+        
+        dp2[0] = nums[1];
+        dp2[1] = max(nums[2],dp2[0]);
+        
+        for(int i=2;i<n-1;i++)
+        {
+            dp1[i] = max(dp1[i-2]+nums[i],dp1[i-1]);
+            dp2[i] = max(dp2[i-2]+nums[i+1],dp2[i-1]);
+        }
+        
+        return max(dp1[n-2],dp2[n-2]);
+    }
+};
