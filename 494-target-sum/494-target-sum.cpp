01d8885a239429ea1d35941d4c583073bb5d9ce9
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2022-06-18 17:33:24 +0530

    Time: 21 ms (62.26%), Space: 9.1 MB (61.43%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,45 +1,51 @@
 class Solution {
 public:
-    int f(int ind,int sum,vector<int>& nums,vector<vector<int>> &dp)
+    int countSubsetSum(vector<int>& nums, int w)
     {
-        if(ind==0)
+        int n= nums.size();
+        int dp[n+1][w+1];
+        
+        for(int i=0;i<n+1;i++)
         {
-            if(sum==0&&nums[ind]==0)
-                return 2;
-            if(sum==0||nums[ind]==sum)
-                return 1;
-            return 0;
+            for(int j=0;j<w+1;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j]=0;
+                }
+                if(j==0)
+                {
+                    dp[i][j]=1;
+                }
+            }
         }
-            
-        // if(ind==0)
-        //     return (nums[ind]==sum);
-        if(ind<0||sum<0)
-            return 0;
         
-        if(dp[ind][sum]!=-1)
-            return dp[ind][sum];
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<=w;j++){
+                dp[i][j]=dp[i-1][j];
+                if(nums[i-1]<=j)
+                    dp[i][j]=dp[i][j]+dp[i-1][j-nums[i-1]];
+            }
+        }
+        return dp[n][w];
         
-        dp[ind][sum] = f(ind-1,sum,nums,dp);
-        if(nums[ind]<=sum)
-            dp[ind][sum] += f(ind-1,sum-nums[ind],nums,dp);
-        return dp[ind][sum];
     }
-    
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        
-        int n = nums.size();
-        
-        for(int i = 0;i<n;i++)
+        int n= nums.size();
+        int sum=0;
+        for(auto x:nums)
         {
-            sum+=nums[i];
+            sum+=x;
         }
         
-        int s1 = (target+sum);
+        int s1 = (target+sum)/2;
+        
+        // int p=(target+s)/2;
+        
         if(sum < target || (sum + target) % 2 != 0)
             return 0;
-        s1/=2;
-        vector<vector<int>> dp(n+1,vector<int>(abs(s1)+1,-1));
-        return f(n-1,abs(s1), nums,dp);
+        
+        return countSubsetSum(nums,abs(s1));
     }
 };
