c19b8c4a488444290921de7adda455958ee2d6f4
Vivek Kumar Ahirwar <ahirwarvivek19@gmail.com>
2022-06-27 10:42:54 +0530

    Time: 80 ms (77.28%), Space: 21.4 MB (47.17%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -0,0 +1,55 @@
+int dx[] = {-1,0,1,0};
+int dy[] = {0,1,0,-1};
+class Solution {
+public:
+    bool isSafe(int x,int y,int n, int m)
+    {
+        if(x>=0&&y>=0&&x<n&&y<m)
+            return true;
+        return false;
+    }
+    
+    int maxDistance(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dist(n,vector<int>(m,INT_MAX));
+        queue<pair<int,int>> q;
+        
+        for(int i = 0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    q.push({i,j});
+                    dist[i][j] = 0;
+                }
+                
+            }
+        }
+        int ans = 0;
+        while(!q.empty())
+        {
+            auto u = q.front();
+            q.pop();
+            int x = u.first;
+            int y = u.second;
+            
+            for(int i =0;i<4;i++)
+            {
+                int newx = x+dx[i];
+                int newy = y+dy[i];
+                if(isSafe(newx,newy,n,m)&&dist[newx][newy]==INT_MAX)
+                {
+                    q.push({newx,newy});
+                    dist[newx][newy] = dist[x][y]+1;
+                    ans = max(ans,dist[newx][newy]);
+                }
+            }
+        }
+        
+        if(ans==0||ans==INT_MAX)
+            return -1;
+        else return ans;
+    }
+};
